ba8f23e3672613b06b843aafb97246ee0210de2e
Gauravmupase09 <upassegaurav30@gmail.com>
2025-08-18 11:13:20 +0530

    Initial commit - FastAPI Patient Management API

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    
+    return data
+
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI is working!"}
+    return {"message": "Patient management System API!"}
+
+@app.get("/about")
+def about():
+    return {"message": "A Fully Functional API To Manage Your Patient Record!"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    
+    return data
